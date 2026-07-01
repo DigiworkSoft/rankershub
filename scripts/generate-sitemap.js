0efe50fd7ef31d712983606c4b0c676a7576ff88
kumbhar-omkar <omkar.m.kumbhar2812@gmail.com>
2026-07-01 19:35:33 +0530

    Hardcode base URL to https://www.rankershub.in in generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -10,7 +10,7 @@ const { create } = require('xmlbuilder2');
 
 // ----- Configuration -------------------------------------------------------
 // Base URL of the site (including protocol and domain). Update this if needed.
-const BASE_URL = (process.env.BASE_URL && !process.env.BASE_URL.includes('localhost')) ? process.env.BASE_URL : 'https://www.rankershub.in';
+const BASE_URL = 'https://www.rankershub.in';
 
 // List of locale strings for hreflang. Example: ['en-US', 'fr-FR']
 // Keep empty array if you do not need language/region specific URLs.
